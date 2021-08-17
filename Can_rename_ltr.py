205e1c671d31a3584ad704889c95f4b4057faba6
Alfredddddddddd <alfred_tay_chee_heng@connect.ite.edu.sg>
2021-08-17 17:34:20 +0800

    abc

--- Can_rename_ltr.py
+++ Can_rename_ltr.py
@@ -39,3 +39,12 @@ class NewSpider(scrapy.Spider):
                 response.urljoin(next_page),
                 callback=self.parse
             )
+
+import unittest
+
+class Can_rename_ltr(unittest.TestCase):
+
+    def test_EngineType(self):
+        print("Testing")
+    if __name__=='__main__':
+       unittest.main()
