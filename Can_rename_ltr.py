3d17a631652944205c9072225fe101d2f3420863
Ace <acechoo29@gmail.com>
2021-08-17 16:13:43 +0800

    Comment thing

--- Can_rename_ltr.py
+++ Can_rename_ltr.py
@@ -1,25 +1,41 @@
 import requests
 
-headers ={'User-Agent':'Mobile'}
-
-url = 'https://www.httpbin.org/headers'
-
+url = 'http://www.ite.edu.sg'
 r = requests.get(url)
+print(r.text)
 
-rh = requests.get(url, headers=headers)
-
-
-print(rh.text)
-
-print("Status code:")
-print("\t *", r.status_code)
+print("Status code:") #get status code
+print("/t*", r.status_code)
 
 h = requests.head(url)
 print("Header:")
-print("******")
+
 for x in h.headers:
-    print("\t", x, ":", h.headers[x])
-print("*******************************")
+    print("/t", x, ":", h.headers[x])
 
-print('Hello World')
+headers = {
+    'User-Agent': 'Mobile'
+}
 
+rh = requests.get(url, headers=headers)
+print(rh.request.headers)
+
+import scrapy
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ["https://www.ite.edu.sg"]
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield{
+                'image LInk': x.xpath(newsel).extract_first()
+            }
+        Page_selector = '.next a::attr(href)'
+        next_page = response.css(Page_selector).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
