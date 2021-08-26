ed79ea2f26544e93eb4137348b8a3e15a33352ba
AceChoo <87575681+AceChoo@users.noreply.github.com>
2021-08-26 10:51:35 +0800

    Update Can_rename_ltr.py

--- Can_rename_ltr.py
+++ Can_rename_ltr.py
@@ -39,3 +39,18 @@ class NewSpider(scrapy.Spider):
                 response.urljoin(next_page),
                 callback=self.parse
             )
+            
+import requests
+import unittest
+import Can_rename_ltr as prog
+
+url = 'https://brickset.com/sets/year-1999'
+r = requests.get(url)
+
+class Hello(unittest.TestCase):
+
+    def test_login_requests(self):
+        self.assertEqual(r.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
