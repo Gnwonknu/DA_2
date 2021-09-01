704891896daddb0286670a2bc84a2eedadd0754f
Gnwonknu <87789726+Gnwonknu@users.noreply.github.com>
2021-09-01 12:31:24 +0800

    Adding comments

--- Can_rename_ltr.py
+++ Can_rename_ltr.py
@@ -2,37 +2,42 @@ import requests
 import scrapy
 import unittest
 
+# Selects url
 url = 'https://brickset.com/sets/year-1999'
+# Perform "get" request
 r = requests.get(url)
 print(r.text)
-
+# Display an "OK/200" status
 print("Status code:") #get status code
 print(r.status_code)
-
+# Display the website header
 h = requests.head(url)
 print("Header:")
 
 for x in h.headers:
     print(x, ":", h.headers[x])
-
+# Modify user-agent to "Mobile"
 headers = {
     'User-Agent': 'Mobile'
 }
-
+# Display modified user-agent
 rh = requests.get(url, headers=headers)
 print(rh.request.headers)
-
-
+# Creates a spider
 class NewSpider(scrapy.Spider):
     name = "new_spider"
+    # Selects url
     start_urls = ["https://brickset.com/sets/year-1999"]
     def parse(self, response):
+        # Selects "img"
         css_selector = 'img'
+        # Display all Image Link found
         for x in response.css(css_selector):
             newsel = '@src'
             yield{
                 'image Link': x.xpath(newsel).extract_first()
             }
+        # To recurse next page    
         Page_selector = '.next a::attr(href)'
         next_page = response.css(Page_selector).extract_first()
         if next_page:
@@ -42,12 +47,13 @@ class NewSpider(scrapy.Spider):
             )
             
 import Can_rename_ltr as prog
-
+# Selects url
 url = 'https://brickset.com/sets/year-1999'
+# Perform "get" request
 r = requests.get(url)
-
+# Create a unit test
 class Hello(unittest.TestCase):
-
+    # Test if teh status code returns "OK/200"
     def test_login_requests(self):
         self.assertEqual(r.status_code, 200)
 
